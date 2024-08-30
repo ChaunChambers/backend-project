2da3365c5a38e7cb4688a3d3ed90b37deca5dde8
ChaunChambers <chaunchambers@gmail.com>
2024-08-30 16:44:22 +0100

    Advanced express Router implemented

--- app.js
+++ app.js
@@ -1,37 +1,10 @@
 const express = require("express");
 const app = express();
-const {
-  getAllTopics,
-  getApi,
-  getArticleByArticleId,
-  getAllArticles,
-  getCommentsByArticleId,
-  postCommentByArticleId,
-  updateArticleById,
-  deleteByCommentId,
-  getAllUsers,
-} = require("./controllers/news.controller");
 const { getErrorHandler } = require("./error_handling/errors");
 
 app.use(express.json());
 
-app.get("/api/topics", getAllTopics);
-
-app.get("/api", getApi);
-
-app.get("/api/articles/:article_id", getArticleByArticleId);
-
-app.get("/api/articles", getAllArticles);
-
-app.get("/api/articles/:article_id/comments", getCommentsByArticleId);
-
-app.get("/api/users", getAllUsers);
-
-app.post("/api/articles/:article_id/comments", postCommentByArticleId);
-
-app.patch("/api/articles/:article_id", updateArticleById);
-
-app.delete("/api/comments/:comment_id", deleteByCommentId);
+app.use("/api", require("./routes/api"));
 
 app.use(getErrorHandler);
 
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTopics,
+  getApi,
+  getArticleByArticleId,
+  getAllArticles,
+  getCommentsByArticleId,
+  postCommentByArticleId,
+  updateArticleById,
+  deleteByCommentId,
+  getAllUsers,
+} = require("../controllers/news.controller");
+
+router.route("/").get(getApi);
+
+router.route("/articles").get(getAllArticles);
+
+router.route("/topics").get(getAllTopics);
+
+router.route("/users").get(getAllUsers);
+
+router
+  .route("/articles/:article_id")
+  .get(getArticleByArticleId)
+  .patch(updateArticleById);
+
+router
+  .route("/articles/:article_id/comments")
+  .get(getCommentsByArticleId)
+  .post(postCommentByArticleId);
+
+router.route("/comments/:comment_id").delete(deleteByCommentId);
+
+module.exports = router;
