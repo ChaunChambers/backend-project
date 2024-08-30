eef86b52b67efc82d832fe169123c86e84a9337a
ChaunChambers <chaunchambers@gmail.com>
2024-08-30 12:54:55 +0100

    Task 12 complete, comment_count included in article_id query

--- check-tables.sql
+++ check-tables.sql
@@ -40,6 +40,9 @@
 --     ('new user','new comment',9)  RETURNING *;
 -- -- SET foreign_key_checks = 1;
 
-SELECT COUNT (comments.article_id) AS comment_count, articles.title, articles.topic, articles.author, articles.created_at, articles.votes,articles.article_img_url, comments.article_id FROM articles JOIN comments ON articles.article_id = comments.article_id
-WHERE topic = 'mitch'
-ORDER BY created_at DESC
+-- SELECT COUNT (comments.article_id) AS comment_count, articles.title, articles.topic, articles.author, articles.created_at, articles.votes,articles.article_img_url, comments.article_id FROM articles JOIN comments ON articles.article_id = comments.article_id
+-- WHERE topic = 'mitch'
+-- GROUP BY articles.title, articles.topic, articles.author, articles.created_at, articles.votes,articles.article_img_url, comments.article_id
+-- ORDER BY created_at DESC
+
+SELECT COUNT (comments.article_id) AS comment_count, articles.title, articles.topic, articles.author, articles.created_at, articles.votes,articles.article_img_url, comments.article_id FROM articles JOIN comments ON articles.article_id = comments.article_id WHERE articles.article_id = '1' GROUP BY articles.title, articles.topic, articles.author, articles.created_at, articles.votes,articles.article_img_url, comments.article_id;
